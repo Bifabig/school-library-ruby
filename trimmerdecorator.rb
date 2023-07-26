7e48272d34da68f3f6084339cf5a39ff0040e482
Bifabig <biftubig@gmail.com>
2023-07-26 11:07:21 +0300

    fix linters issue

--- trimmerdecorator.rb
+++ trimmerdecorator.rb
@@ -3,6 +3,7 @@ require_relative 'decorator'
 class TrimmerDecorator < Decorator
   def correct_name
     return @nameable.correct_name[0, 10] if @nameable.correct_name.length > 10
+
     @nameable.correct_name
   end
 end
