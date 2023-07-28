20c17bf14ed3845f18d1b88abfe8117b4917da26
Bifabig <biftubig@gmail.com>
2023-07-28 10:40:05 +0300

    remove rentals

--- person.rb
+++ person.rb
@@ -22,8 +22,7 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(book, date, rental)
-    @rentals.push(rental)
+  def add_rental(book, date)
     Rental.new(date, book, self)
   end
 
