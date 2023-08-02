51b1a44f74275078867b88590ca5864108e30b29
Bifabig <biftubig@gmail.com>
2023-08-02 17:14:00 +0300

    modify book file to include to_json and json_create methods

--- book.rb
+++ book.rb
@@ -5,6 +5,7 @@ class Book
   attr_reader :rentals
 
   def initialize(title, author)
+    @id = Random.rand(1..1000)
     @title = title
     @author = author
     @rentals = []
@@ -12,5 +13,20 @@ class Book
 
   def add_rental(person, date)
     Rental.new(date, person, self)
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'title' => @title,
+      'author' => @author
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    book = new(object['title'], object['author'])
+    book.instance_variable_set(:@id, object['id'])
+    book
   end
 end
